4fa7d4516424fd7cc4d02ae03cd275f078d430de
Guilherme Pereira de Oliveira <163172490+guipoliver@users.noreply.github.com>
2024-08-14 14:55:18 -0300

    Delete lista 2 sala directory

--- lista 2 sala/questao 7.py
+++
@@ -1,8 +0,0 @@
-numero = int(input("Digite um número inteiro (positivo ou negativo): "))
-
-if numero < 0:
-    valor_absoluto = -numero
-else:
-    valor_absoluto = numero
-
-print(f"O valor absoluto de {numero} é {valor_absoluto}.")
