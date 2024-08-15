375b1c85cb9dac995a3a839cac587c13648d8fbb
Guilherme_Pereira <Guilherme321890@gmail.com>
2024-08-15 00:12:00 -0300

    atualizacao das listas

--- lista casa/circunferencia.py
+++
@@ -1,5 +0,0 @@
-r = input('Digite o raio de uma circunferencia:')
-r = float(r)
-A = 3,14 * r ** 2
-C = 2 * 3,14 * r
-print(f'A area da circunferencia é igual a {A:.2f} e o comprimento da circunferencia é igual a {C:.2f}')
--- lista casa/retangulo.py
+++
@@ -1,7 +0,0 @@
-a = input('digite o tamanho do lado a do retangulo:')
-a = float(a)
-b = input('digite o tamanho do lado b do retangulo:')
-b = float(b)
-A = a * b
-P = 2 * a + 2* b
-print(f'a area do retangulo é {A} e o perimetro do retangulo é igual a: {P}')
